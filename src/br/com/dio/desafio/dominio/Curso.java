c2845cd55a0e2a768dd0c1ebd45603b8ccaf88dd
Monique Moura <eumoniqueribeiro@gmail.com>
2024-07-28 22:48:40 -0300

    Final

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,31 +1,17 @@
 package br.com.dio.desafio.dominio;
 
+public class Curso extends Conteudo{
 
-public class Curso {
-        private String titulo;
-        private String descricao;
-        private int cargaHoraria;
+    private int cargaHoraria;
 
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
 
     public Curso() {
     }
 
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public int getCargaHoraria() {
         return cargaHoraria;
